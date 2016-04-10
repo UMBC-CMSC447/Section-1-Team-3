387b503c9472e484c7e2d8400fb8e5c3192ef998
Matthew Walker <walkmat1@umbc.edu>
2016-04-10 12:55:25 -0400

    Added a basic model type for testing

--- Code/mysite/polls/models.py
+++ Code/mysite/polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class property(models.Model):
+    Name = models.CharField(max_length = 30)
+    Price = models.FloatField()
+    Location = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.Name
