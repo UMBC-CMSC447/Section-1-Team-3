70323eeb10d1b25085f7106a320b02c5ebc1a6c1
Matthew Walker <walkmat1@umbc.edu>
2016-04-12 11:54:34 -0400

    Display username on user page

--- Code/mysite/beach_homepage/views.py
+++ Code/mysite/beach_homepage/views.py
@@ -28,4 +28,4 @@ def index(request):
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response(newPath, context_dict, context)
+    return render_to_response(newPath, {'user' : request.user}, context)
