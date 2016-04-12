b419b9e290dfb1d16e528ad0126eacddda1ce6bf
Matthew Walker <walkmat1@umbc.edu>
2016-04-12 12:41:22 -0400

    Trying to display properties

--- Code/mysite/beach_homepage/views.py
+++ Code/mysite/beach_homepage/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-
+from polls.models import property
 # Create your views here.
 from django.http import HttpResponse
 
@@ -24,8 +24,8 @@ def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     context_dict = {'boldmessage': "I am bold font from the context"}
-
+    test = property.objects.all()
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response(newPath, {'user' : request.user}, context)
+    return render_to_response(newPath, {'user' : request.user,'house' : test}, context)
