c2f1bf376cc7d99685886dcd233d5ffccc65957d
Matthew Walker <walkmat1@umbc.edu>
2016-04-13 16:35:23 -0400

    A few channges to upload

--- Code/mysite/upload/models.py
+++ Code/mysite/upload/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class ProfileImage(models.Model):
-    image = models.FileField(upload_to='profile/%Y/%m/%d')
+    image = models.FileField(upload_to='profile/')
