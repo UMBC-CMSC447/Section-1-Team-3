148659a0d25a8a153db37632327ffc1fba063b78
Matthew Walker <walkmat1@umbc.edu>
2016-05-04 17:21:39 -0400

    Populated the database with all of the autogenerated properties. Wrote a cool script to do it to

--- Code/mysite/generate.cpp
+++ Code/mysite/generate.cpp
@@ -3,25 +3,37 @@
 #include <time.h>
 #include <fstream>
 #include <sqlite3.h>
+#include <math.h>
 using namespace std;
 
 int main () {
   ofstream myfile;
   float price;
-  int rating;
+  float lat = 38.386983;
+  float Long = -75.064957;
+  float flip;
+  string loc = "Ocean-City";
   string styles[] = {"Villa", "Modern", "Cottage", "Condo", "Ranch", "Victorian"};
-  myfile.open ("dataset.txt");
-  for (int i = 0; i < 10; i++){
-    rating = rand() % 5;
+  string filename = loc + ".txt";
+  const char* file = filename.c_str();
+  myfile.open (file);
+  for (int i = 0; i < 23; i++){
+    flip = rand()%2;
+    float variance = ((float)rand())/(float)((RAND_MAX/10.0));
+    variance /= 1000.0;
+    if (flip > 0){
+      variance *= -1;
+    }
     price = (rand() % 10 + 1) * 10000;
-    myfile << "{ \n";
-    myfile << "image : \"image0"  << i << ".png\", \n";
-    myfile << "style : \""  << styles[i%6] << "\", \n";
-    myfile << "timeslot : \""  << i << "\", \n";
-    myfile << "price : \""  << price << "\", \n";
-    myfile << "rating : \""  << rating << "\", \n";
-    myfile << "} \n";
+    myfile << loc <<"house0" << i << "\n";
+    myfile << price << "\n";
+    myfile << loc << "\n";
+    myfile << "ImageTest0" << abs(rand()%25) << ".jpg" << "\n";
+    myfile << "This is example " << loc <<" house number " << i << " in " << loc << "\n";
+    myfile << lat+variance << "\n";
+    myfile << Long+variance << "\n";
   }
+  
   myfile.close();
   return 0;
 }
